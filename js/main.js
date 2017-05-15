0d7b077eb666ffa94008aea6582175a898d8244d
Pola <Pola.kinomoto@gmail.com>
2017-05-15 14:03:26 -0400

    ejercicio bicicleta

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+function datos(nombre, apellido, email, contraseña){
+if(nombre == ""|| apellido=="" || email== ""|| contraseña==""){
+	alert("Ingresa datos por favor");
+}	
+	
+};
+
+
 function validateForm(){
-	/* Escribe tú código aquí */
-}
+var nombre = document.getElementById("name").value;
+var apellido = document.getElementById("lastname").value;
+var email = document.getElementById("input-email").value;
+var contraseña= document.getElementById("input-password").value;
+var twitter= document.getElementById("input-social").value;
+var button= document.getElementById("boton")[0];
+
+datos(nombre, apellido, email, contraseña, button)
+};
+
+document.getElementById("boton").addEventListener("click", function(){
+});
+
+
+
+
